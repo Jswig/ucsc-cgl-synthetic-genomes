112ef17def23620fa8ce9af5bdc3e33f0673ebd0
Anders Poirel <andersjopo@gmail.com>
2020-11-08 18:54:48 -0800

    script and step for using haplo

--- src/vcf_to_haplotype/get_haplotype.sh
+++ src/vcf_to_haplotype/get_haplotype.sh
@@ -1,4 +1,7 @@
 #!bin/sh
 
-haplo --database
-haplo --database 
+praw="data/raw/"
+pinterim="data/interim/"
+
+haplo --database -i "${praw}f8_chr13_brca2_copd_hmb.vcf" -o "${pinterim}chr13_brca2_haplo.tsv"
+haplo --database -i "${praw}f8_chr17_brca1_copd_hmb.vcf" -o "${pinterim}chr17_brca1_haplo.tsv"
