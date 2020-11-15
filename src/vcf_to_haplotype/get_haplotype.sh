238799e8a0f286ea149eaa2e23f7266ffa89e978
Anders Poirel <andersjopo@gmail.com>
2020-11-15 00:14:02 -0800

    add gatk step

--- src/vcf_to_haplotype/get_haplotype.sh
+++ src/vcf_to_haplotype/get_haplotype.sh
@@ -3,5 +3,12 @@
 praw="data/raw/"
 pinterim="data/interim/"
 
-haplo --database -i "${praw}f8_chr13_brca2_copd_hmb.vcf" -o "${pinterim}chr13_brca2_haplo.tsv"
-haplo --database -i "${praw}f8_chr17_brca1_copd_hmb.vcf" -o "${pinterim}chr17_brca1_haplo.tsv"
+haplo --database \
+    -i "${praw}f8_chr13_brca2_copd_hmb.vcf" \
+    -o "${pinterim}chr13_brca2_haplo.tsv" \
+    --dont_export
+
+haplo --database \
+    -i "${praw}f8_chr17_brca1_copd_hmb.vcf" \
+    -o "${pinterim}chr17_brca1_haplo.tsv" \
+    --dont-export
