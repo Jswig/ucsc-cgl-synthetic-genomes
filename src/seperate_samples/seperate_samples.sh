ef1e9be50680a2f47826f30b0e298beab45ec34c
Anders Poirel <andersjopo@gmail.com>
2020-11-16 14:54:33 -0800

    code for seperating samples

--- src/seperate_samples/seperate_samples.sh
+++ src/seperate_samples/seperate_samples.sh
@@ -6,5 +6,12 @@ extract_sample_names() {
         sed '/^#/d' > "{$2}{$1}.txt"
 }
 
-extract_sample_names $1 $2
-extract_sample_names $1 $3
+extract_samples() {
+    for sample in `bcftools query -l $1` 
+    do
+        bcftools view -c1 -Oz -s $sample -o "$2/$sample.vcf" $1
+    done
+}
+
+extract_samples $1 $2
+extract_samples $3 $4
