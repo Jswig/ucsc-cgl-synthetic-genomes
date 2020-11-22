59ed99db0945861b33eecf7093a8b7a09769fb03
Anders Poirel <andersjopo@gmail.com>
2020-11-22 15:57:14 -0800

    remove unnecessary sample name extraction

--- src/seperate_samples/seperate_samples.sh
+++ src/seperate_samples/seperate_samples.sh
@@ -1,11 +1,5 @@
 #!bin/sh
 
-extract_sample_names() {
-    bcftools view -h $1 | 
-        cut -s -f 10- | 
-        sed '/^#/d' > "{$2}{$1}.txt"
-}
-
 extract_samples() {
     for sample in `bcftools query -l $1` 
     do
