010e3c5204d38288452b8c6691487deed4d9ce10
Anders Poirel <andersjopo@gmail.com>
2020-12-07 03:46:12 -0800

    sequence constructor works

--- src/preprocessing/extract_sequences.py
+++ src/preprocessing/extract_sequences.py
@@ -4,26 +4,58 @@ import allel
 from Bio import SeqIO
 
 
-def extract_from_vcf(reference_fasta: str, variants_vcf: str, start: int, end: int):
-    
-    reference = list(SeqIO.parse(reference_fasta, "fasta"))
-    variants = allel.read_vcf(variants_vcf, fields=["POS", "REF", "ALT"])
-
+def extract_from_vcf(
+    reference_fasta: str, 
+    variants_vcf: str, 
+    sample_ids: str, 
+    start: int, 
+    end: int
+):
+    reference = SeqIO.read(reference_fasta, "fasta")
     reference_seq = str(reference.seq)
     reference_gene = reference_seq[start:end]
 
+    ids = pd.read_csv(sample_ids, header=None)
+    ids = ids[0].tolist()
+
+
+    variants = (allel
+        .vcf_to_dataframe(variants_vcf, fields=['POS', 'REF', 'ALT'])
+        .drop(['ALT_2', 'ALT_3'], axis=1) # ALT_2, ALT_3 are always empty
+    )
+    # shift POS to start from 0 in the gene's sequence
+    variants['POS'] = variants['POS'] - start
+
+    genotypes = allel.read_vcf(variants_vcf, fields=['POS'], samples=ids)
+    genotypes = genotypes['calldata/GT']
+    haplo_1 = pd.DataFrame(genotypes[:,:,0])
+    haplo_2 = pd.DataFrame(genotypes[:,:,1])
+
+    haplos = pd.concat([variants, haplo_1, haplo_2], axis=1)
+    haplos = haplos[(haplos['REF'].str.len() == 1) & (haplos['ALT_1'].str.len() == 1)]
+
+    # iterate over all haplotype columns
+    for j in range(3, haplos.shape[1]):
+        seq = list(reference_seq) # cannot modify a string
+        for i in range(len(haplos)):
+            if haplos.iloc[i,j] == 1:
+                seq[haplos.loc[i, 'POS']] = haplos.loc[i, 'ALT_1']
+        
+    # TODO: dump to file
 
 if __name__ == "__main__":
 
     extract_from_vcf(
         reference_fasta="data/Homo_sapiens.GRCh38.dna.chromosome.17.fa", 
         variants_vcf="data/raw/bcra1.vcf",
+        sample_ids="data/interim/sample_names.txt",
         start=43044295,
         end=43170246,
     )
     extract_from_vcf(
         reference_fasta="data/Homo_sapiens.GRCh38.dna.chromosome.13.fa", 
         variants_vcf="data/raw/bcra2.vcf",
+        sample_ids="data/interim/sample_names.txt",
         start=32315086,
         end=32400267,
     )
