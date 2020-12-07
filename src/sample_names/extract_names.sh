a3f4b25af218946a4458d53a8e737edd405be8f1
Anders Poirel <andersjopo@gmail.com>
2020-12-06 19:42:29 -0800

    consistent output file names

--- src/sample_names/extract_names.sh
+++ src/sample_names/extract_names.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-if [ ! -f data/interim/brca_1_sample_names.txt ]
+if [ ! -f data/interim/brca1_sample_names.txt ]
 then
     bcftools query -l data/raw/brca1.vcf > data/interim/brca1_sample_names.txt
 fi 
 
-if [ ! -f data/interim/brca_2_sample_names.txt ]
+if [ ! -f data/interim/brca2_sample_names.txt ]
 then
     bcftools query -l  data/raw/brca2.vcf > data/interim/brca2_sample_names.txt
 fi 
