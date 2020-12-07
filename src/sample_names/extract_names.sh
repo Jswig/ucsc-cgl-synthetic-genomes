2418ee711a410dca3cca5c06caa2b1a1815affa7
Anders Poirel <andersjopo@gmail.com>
2020-12-06 19:54:14 -0800

    sample ID same for both brca1 and brca2 so no need to extract them twice

--- src/sample_names/extract_names.sh
+++ src/sample_names/extract_names.sh
@@ -1,11 +1,6 @@
 #!bin/bash
 
-if [ ! -f data/interim/brca1_sample_names.txt ]
+if [ ! -f data/interim/sample_names.txt ]
 then
-    bcftools query -l data/raw/brca1.vcf > data/interim/brca1_sample_names.txt
-fi 
-
-if [ ! -f data/interim/brca2_sample_names.txt ]
-then
-    bcftools query -l  data/raw/brca2.vcf > data/interim/brca2_sample_names.txt
+    bcftools query -l data/raw/brca1.vcf > data/interim/sample_names.txt
 fi 
