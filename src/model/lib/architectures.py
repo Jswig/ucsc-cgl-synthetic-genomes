a24c4fd5c7204b49f0236cc81ea855cfcf42e26d
Anders Poirel <andersjopo@gmail.com>
2021-02-08 12:01:01 -0800

    add batch normalization to model

--- src/model/lib/architectures.py
+++ src/model/lib/architectures.py
@@ -1,30 +1,32 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 class ResidualBlock(nn.Module):
     def __init__(self, d_in, d_out, relu): #pick nn.ReLU for generator, nn.LeakyRelu for discriminator
         super().__init__()
+        self.bnorm_1 = nn.BatchNorm1d(d_in)
         self.relu_1 = relu()
         self.conv_1 = nn.Conv1d(in_channels=d_in, out_channels=d_out, kernel_size=5, padding = 2)
         self.relu_2 = relu()
+        self.bnorm_2 = nn.BatchNorm1d(d_in)
         self.conv_2 = nn.Conv1d(in_channels=d_in, out_channels=d_out, kernel_size=5, padding = 2)
         # bias not needed, already present by default in Conv1d()
         
     def forward(self, x):
-        y = self.relu_1(x)
+        y = self.bnorm_1(x)
+        y = self.relu_1(y)
         y = self.conv_1(y)
+        y = self.bnorm_1(y)
         y = self.relu_2(y)
         y = self.conv_2(y)
         return x + (0.3 * y)
+
 
 class Generator(nn.Module):
     def __init__(self, seq_len, batch_size, latent_dim=100):
         super(Generator, self).__init__()
         # layers
+        self.bnorm_in = nn.BatchNorm1d(latent_dim)
         self.linear_in = nn.Linear(in_features=latent_dim, out_features=latent_dim*seq_len)
         self.resblocks = nn.Sequential(
-            *[ResidualBlock(100, 100, relu=nn.ReLU) for _ in range(5)]
+            *[ResidualBlock(latent_dim, latent_dim, relu=nn.LeakyReLU) for _ in range(5)]
         )
         self.conv_out = nn.Conv1d(in_channels=latent_dim, out_channels=4, kernel_size=1)
         # hyper-parameters
@@ -33,7 +35,8 @@ class Generator(nn.Module):
         self.batch_size = batch_size
 
     def forward(self,z):
-        y = self.linear_in(z)    
+        y = self.bnorm_in(z)
+        y = self.linear_in(y)    
         y = torch.reshape(y, (self.batch_size, self.latent_dim, self.seq_len))
         y = self.resblocks(y)
         y = self.conv_out(y)
@@ -47,10 +50,11 @@ class Discriminator(nn.Module):
         # layers
         self.conv_in = nn.Conv1d(in_channels=4, out_channels=latent_dim, kernel_size=1)
         self.resblocks = nn.Sequential(
-            *[ResidualBlock(latent_dim, latent_dim, relu=nn.ReLU) for _ in range(5)]
+            *[ResidualBlock(latent_dim, latent_dim, relu=nn.LeakyReLU) for _ in range(5)]
         )
+        self.bnorm_out = nn.BatchNorm1d(latent_dim*seq_len)
         self.linear_out = nn.Linear(in_features=latent_dim*seq_len, out_features=1)
-        # hyper-parameter
+        # hyper-parameters
         self.seq_len = seq_len
         self.latent_dim = latent_dim
         self.batch_size = batch_size
@@ -59,5 +63,6 @@ class Discriminator(nn.Module):
         y = self.conv_in(x)
         y = self.resblocks(y)
         y = torch.reshape(y, (self.batch_size, self.latent_dim*self.seq_len))
+        y = self.bnorm_out(y)
         y = self.linear_out(y)
         return y
