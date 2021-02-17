bc93e2104b853a5ed375e2b68e12ef83c611ff47
Anders Poirel <andersjopo@gmail.com>
2021-02-16 16:03:19 -0800

    first pass at generating sample frequencies model

---
+++ docs/sampling_model.md
@@ -0,0 +1,24 @@
+
+
+Create a doubly dictionary of the form
+```json
+{
+    1 : {
+        "vals": ["A", "T"],
+        "prob": [0.33, 0.66]
+    }
+    324 : {
+        "vals" : ["A", "C", "G"],
+        "probs": [0.5, 0.25, 0.25]
+    } 
+    ...
+}
+```
+for each element of the dict, use 
+```python
+np.choice(vals, n_samples, prob)
+```
+Then reconstruct sequences using similar method to previous
+
+Can also straighforwardly convert to VCF using just the variants information 
+(loop over the variants)
---
+++ src/model/lib/sample_freqs.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np 
+import allel
+import os
+import sys
+import json
+
+def compute_sample_freqs(
+    variants_vcf: str, 
+    output: str,
+):
+    variants = (allel
+        .vcf_to_dataframe(variants_vcf, fields=['POS', 'REF', 'ALT'])
+        .drop(['ALT_2', 'ALT_3'], axis=1) # ALT_2, ALT_3 are always empty
+    )
+    # shift POS to start from 0 in the gene's sequence
+    variants['POS'] = variants['POS'] - start
+
+    genotypes = allel.read_vcf(variants_vcf, fields=['calldata/GT'])
+    genotypes = genotypes['calldata/GT']
+    haplo_1 = pd.DataFrame(genotypes[:,:,0])
+    haplo_2 = pd.DataFrame(genotypes[:,:,1])
+    
+    num_samples = haplo_1.shape[1] * 2
+
+    haplos = pd.concat([variants, haplo_1, haplo_2], axis=1)
+    haplos = haplos[(haplos['REF'].str.len() == 1) & (haplos['ALT_1'].str.len() == 1)] # subset to only keep SNPs
+    haplos = haplos.reset_index() # reset index after filtering 
+
+    var_freqs = {}
+
+    for pos in haplos['POS'].unique():
+        sum_alt = 0
+        pos_variants = haplos[haplos['POS'] == pos]
+        pos_freqs = {}
+        # frequencies of each variant at this position
+        for row in pos_variants.iterrows():
+            num_alt = row[4:].sum()
+            sum_alt += num_alt 
+            pos_freqs[row['ALT_1']] = num_alt / num_samples
+        # frequency of the reference 
+        pos_freqs[row['REF']] = (num_samples - sum_alt) / num_samples
+
+        # add result to main dictionary
+        var_freqs[pos] = pos_freqs
+
+    with open(
+        os.path.join(output, os.path.splitext(variants_vcf) + '.json'), 
+        'w',
+    ) as fp:
+        json.dump(var_freqs, fp)
+
+    return 
+
+if __name__ == '__main__':
+    for fname in sys.argv[2:]:   
+        compute_sample_freqs(fname, sys.argv[1])
