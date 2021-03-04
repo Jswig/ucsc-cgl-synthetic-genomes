63cf788f313d351108bb22cfdab87dcb353edb66
Anders Poirel <andersjopo@gmail.com>
2021-03-04 14:11:41 -0800

    fix chromosome in generation code

--- src/frequency_generation/generate_naive_samples.py
+++ src/frequency_generation/generate_naive_samples.py
@@ -22,9 +22,9 @@ def generate_samples(freqs_dict: dict, n_samples: int) ->  Deque[np.ndarray]:
         samples.append(
             np.random.choice(v[0], size=n_samples*2, p=v[1])
         ) # n_samples*2 as we need two samples to get the genotype
-    with open('output/samples_log.json', 'w') as log:
-        l_samples = [list(sample) for sample in samples]
-        json.dump(l_samples, log)  
+    # with open('output/samples_log.json', 'w') as log:
+    #     l_samples = [list(sample) for sample in samples]
+    #     json.dump(l_samples, log)  
     return samples
 
 def samples_to_vcf(
@@ -34,7 +34,8 @@ def samples_to_vcf(
         "##fileformat=VCFv4.1\n"
         "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t"
     )   
-    ids = [f'NF{str(k).zfill(5)}\t' for k in range(n_samples)]
+    ids = [f'NF{str(k).zfill(5)}\t' for k in range(n_samples-1)]
+    ids.append(f'NF{str(n_samples-1).zfill(5)}')
     vcf_header = vcf_header + ''.join(ids) + '\n'
 
     with open(output, 'w') as vcf:
@@ -46,7 +47,7 @@ def samples_to_vcf(
             haplos = samples.popleft()
             haplo_1, haplo_2 = np.split(haplos, 2) # haplos has length 2*n_samples
             for variant in bases:
-                vcf.write(f'17\t{pos}\t.\t{ref}\t{variant}\t.\t.\t.\tGT\t')
+                vcf.write(f'13\t{pos}\t.\t{ref}\t{variant}\t.\t.\t.\tGT\t')
                 haplo_1_has_var = np.where(haplo_1 == variant, 1, 0)
                 haplo_2_has_var = np.where(haplo_2 == variant, 1, 0)
                 genotypes = [
@@ -54,7 +55,7 @@ def samples_to_vcf(
                     for k in range(len(haplo_1) - 1)
                 ]
                 genotypes.append(
-                    f'{haplo_1_has_var[len(haplo_1)]}|{haplo_2_has_var[len(haplo_1)]}'
+                    f'{haplo_1_has_var[len(haplo_1)-1]}|{haplo_2_has_var[len(haplo_1)-1]}'
                 )
                 vcf.write(''.join(genotypes))
                 vcf.write('\n')
