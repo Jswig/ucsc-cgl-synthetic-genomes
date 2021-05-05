65dd3b9c0f421b757c77faf8d9d7973c654a6da7
Anders Poirel <andersjopo@gmail.com>
2021-05-05 01:40:51 -0700

    em fit skeleton

--- src/em_smm_fit/fit_smm.py
+++ src/em_smm_fit/fit_smm.py
@@ -9,5 +9,16 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('input', help='Input VCF file')
 
+def fit_em_smm(variants_vcf: str):
+    variants = (allel
+		.vcf_to_dataframe(variants_vcf, fields=['POS', 'REF', 'ALT'])
+		.drop(['ALT_2', 'ALT_3'], axis=1) # ALT_2, ALT_3 are always empty
+	)
+	genotypes = allel.read_vcf(variants_vcf, fields=['calldata/GT'])
+	genotypes = genotypes['calldata/GT']
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
+
+    fit_em_smm(args.input)
