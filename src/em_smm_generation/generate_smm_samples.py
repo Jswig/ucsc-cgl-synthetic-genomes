26a78915a60b2c245bdf533c8d0d91c09ea72888
Anders Poirel <andersjopo@gmail.com>
2021-06-18 04:06:51 -0700

    skeleton for smm sample generation code. NOTE: maybe rework naive model code with numba and all

--- src/em_smm_generation/generate_smm_samples.py
+++ src/em_smm_generation/generate_smm_samples.py
@@ -2,7 +2,9 @@ import argparse
 import numpy as np 
 import pandas as pd 
 
+from os.path import join
 from numba import jit 
+from typing import Dict
 from src.lib.vcf import samples_to_vcf
 
 parser = argparse.ArgumentParser(
@@ -14,12 +16,54 @@ parser.add_argument(
 )
 parser.add_argument('output', help='Output VCF file')
 parser.add_argument(
-    '-n', '--n_samples', default=100, type=int, help='Number of samples to generate'
+    '-n', '--n_samples', default=100, type=int, help='Number of sample genotypes to generate'
 )
+parser.add_argument('s', '--seed', default=42, type=int, help='Random seed')
 
-def generate_samples():
-    pass
+def _generate_samples(
+    n_samples: int,
+    group_probs: np.ndarray,
+    variant_probs: np.ndarray,
+    seed: int,
+) -> Dict[int, np.array]:
+    samples = []
+    
+    rng = np.random.default_rng(seed)
+    
+    n_loci = variant_probs.shape[1]
+    max_n_variants = variant_probs.shape[2]
+    variant_codes = np.arange(max_n_variants)
+    n_groups = len(group_probs)
+
+    haplotypes = np.full((n_samples*2, n_loci), 0)
+
+    groups = rng.choice(
+        np.arange(0, len(group_probs)),
+        size=n_samples*2,
+        p=group_probs,
+    )
+    group_counts = [
+        np.count_nonzero(groups == i)
+        for i in range(K)
+    ] 
+
+    # iterate over groups, generating appropriate number of samples from 
+    # each group
+    for alpha in range(n_groups):
+        for k in range(n_loci):
+            pass
+
+    # NOTE: this returns an array of samples. Maybe convert to a dict at a 
+    # later point for compatibility with existing VCF writing code 
+
+    # shuffle columns of resulting array (not really necessary, just prevents it 
+    # from looking weird)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    pass
+
+    group_e = np.load(join(args.input, 'group_e.npy'))
+    group_probs = np.load(join(args.input, 'group_e.npy'))
+    variant_probs = np.load(join(args.input, 'variant_probs.npy')) 
+    
--- src/naive_generation/generate_naive_samples.py
+++ src/naive_generation/generate_naive_samples.py
@@ -3,7 +3,7 @@ import argparse
 import json
 import numpy as np 
 from tqdm import tqdm
-from typing import Dict, Tuple
+from typing import Dict
 
 from src.lib.vcf import samples_to_vcf
 
@@ -13,14 +13,12 @@ parser = argparse.ArgumentParser(
 parser.add_argument('input', help='Sample variant frequencies in JSON format')
 parser.add_argument('output', help='Output VCF file')
 parser.add_argument(
-	'-n', '--n_samples', default=100, type=int, help='Number of samples to generate'
+	'-n', '--n_samples', default=100, type=int, help='Number of sample genotypes to generate'
 )
 
 def generate_samples(
 	freqs_dict: dict, 
 	n_samples: int, 
-	log: bool = False,
-	log_path: str = None,
 ) ->  Dict[int, np.ndarray]:
 	samples = {}
 	for pos, v in tqdm(freqs_dict.items()):
@@ -29,10 +27,6 @@ def generate_samples(
 			size=n_samples*2, 	
 			p=v['freq']
 		)
-	if log:
-		with open(log_path, 'w') as log:
-			l_samples = {k: list(v) for (k,v) in samples	}
-			json.dump(l_samples, log)  
 	return samples
 
 if __name__ == '__main__':
